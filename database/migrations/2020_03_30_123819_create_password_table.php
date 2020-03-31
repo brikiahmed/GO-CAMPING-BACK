0d92e0a3e408d1b532ede08474a54ad8760852f0
Ahmed <briki.ahmed21@gmail.com>
2020-03-31 20:49:23 +0200

    first commit

---
+++ database/migrations/2020_03_30_123819_create_password_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePasswordTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('password', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('password' , function (Blueprint $table){
+            $table->increments('id');
+            $table->string('email')->index();
+            $table->string('token');
+            $table->timestamps();}
+        );
+    }
+}
